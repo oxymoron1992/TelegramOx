4617485e22a64d6c3aef03601c2211257a03b805
oxymoron1992 <oxymoron.dr@gmail.com>
2017-10-13 13:04:30 +0400

    11

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
    include('vendor/autoload.php'); //Подключаем библиотеку
    use Telegram\Bot\Api; 
  
-   $telegram = new Api('375466075:AAEARK0r2nXjB67JiB35JCXXhKEyT42Px8s'); //Устанавливаем токен, полученный у BotFather
+   $telegram = new Api('437902177:AAEeJ4LW9IyypuDNdPfvKJfZKUTpwGWSHq4'); //Устанавливаем токен, полученный у BotFather
    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
    
    $text = $result["message"]["text"]; //Текст сообщения
